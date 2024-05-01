6b4f35a8ced45fe6730f2833ea67807958df31e6
PrinceAsia <princeasia013@gmail.com>
2024-05-01 16:26:15 +0500

    1.3 - Simple JWT added (no migrated)

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Simple JWT
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 # Swagger
 from django.urls import re_path
 from rest_framework import permissions
@@ -30,6 +36,8 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
