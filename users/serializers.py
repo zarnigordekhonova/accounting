a3dda2c023151d396721eb2969c6aaee2e9242bc
PrinceAsia <princeasia013@gmail.com>
2024-05-04 05:56:38 +0500

    1.9 - Password validator added to users.serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 
@@ -6,7 +7,7 @@ UserModel = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, validators=[validate_password])
 
     def create(self, validated_data):
         user = UserModel.objects.create_user(**validated_data)
